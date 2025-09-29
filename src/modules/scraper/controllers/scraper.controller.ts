6b5857bf3e802de53285f87786daad0e5923bf66
Carolina Moutella <emaildacarolmoutella@gmail.com>
2025-09-29 14:48:49 -0300

    refact(scraper): add authentication to request

--- src/modules/scraper/controllers/scraper.controller.ts
+++ src/modules/scraper/controllers/scraper.controller.ts
@@ -1,5 +1,14 @@
-import { Controller, Get, Query, UseInterceptors } from '@nestjs/common'
+import {
+  Controller,
+  Get,
+  InternalServerErrorException,
+  Query,
+  UnauthorizedException,
+  UseInterceptors
+} from '@nestjs/common'
 import { ApiResponse, ApiTags } from '@nestjs/swagger'
+import { AuthService } from '@src/modules/auth/services/auth.service'
+import { ApplicationUserService } from '@src/modules/user/services/application-user.service'
 import { LoggingInterceptor } from 'src/shared/interceptors/logging.interceptor'
 
 import { ScrapedAdData } from '../schemas/scraper.schema'
@@ -8,6 +17,11 @@ import { ScrapedAdData } from '../schemas/scraper.schema'
 @UseInterceptors(LoggingInterceptor)
 @Controller('scraper')
 export class ScraperController {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: ApplicationUserService
+  ) {}
+
   @ApiResponse({
     type: ScrapedAdData,
     status: 200,
@@ -15,26 +29,62 @@ export class ScraperController {
   })
   @Get('/')
   async scrapeAd(@Query() { url }: { url: string }) {
-    const scraperService = process.env.SCRAPER_SERVICE
-
-    if (!scraperService) {
-      console.error('secret SCRAPER_SERVICE not set')
-    }
-
     try {
+      const scraperService = process.env.SCRAPER_SERVICE
+      if (!scraperService) {
+        console.error('secret SCRAPER_SERVICE not set')
+        throw new Error()
+      }
+
+      const scraperClient = process.env.SCRAPER_APP_CLIENT
+      const scraperKey = process.env.SCRAPER_APP_KEY
+      if (!scraperClient || !scraperKey) {
+        console.error('secret SCRAPER_APP_CLIENT or SCRAPER_APP_KEY not set')
+        throw new UnauthorizedException()
+      }
+
+      const app = await this.userService.getByName(scraperClient)
+
+      if (!app || app.role !== 'app') {
+        throw new UnauthorizedException('Credenciais inválidas')
+      }
+
+      const data = await this.authService.authenticateApplication(app, {
+        email: app.email,
+        password: scraperKey
+      })
+
+      if (!data.token) {
+        throw new UnauthorizedException(
+          'Não foi possível autenticar suas credenciais'
+        )
+      }
+
+      const headers = new Headers()
+      headers.append('Authorization', `Bearer ${data.token}`)
+
       const response = await fetch(`${scraperService}/ad/?url=${url}`, {
-        method: 'GET'
+        method: 'GET',
+        headers
       })
 
       if (!response.ok) {
-        throw new Error('Scraper service error')
+        if (response.status === 401) {
+          throw new UnauthorizedException()
+        }
+
+        throw new InternalServerErrorException('Scraper service error')
       }
 
       const adData = await response.json()
 
       return adData
-    } catch (_err) {
-      return { status: 500, message: 'Scraper error' }
+    } catch (err) {
+      if (err instanceof UnauthorizedException) {
+        throw err
+      }
+
+      throw new InternalServerErrorException('Erro interno do servidor')
     }
   }
 }
