de7fe1e5236fa05aded7ec16c8a519ca30a5e1f2
Carolina Moutella <emaildacarolmoutella@gmail.com>
2025-10-15 12:19:16 -0300

    put back

--- src/shared/pipe/zod-validation.pipe.ts
+++ src/shared/pipe/zod-validation.pipe.ts
@@ -1,14 +1,10 @@
 import type { PipeTransform } from '@nestjs/common'
 import { BadRequestException } from '@nestjs/common'
-import type { AppService } from '@src/services/app.service'
 import type { ZodSchema } from 'zod'
 import { ZodError } from 'zod'
 
 export class ZodValidationPipe implements PipeTransform {
-  constructor(
-    private readonly schema: ZodSchema,
-    private readonly appService: AppService
-  ) {}
+  constructor(private schema: ZodSchema) {}
 
   transform(value: unknown) {
     try {
@@ -20,14 +16,12 @@ export class ZodValidationPipe implements PipeTransform {
         const zodErrors = []
         err.issues.forEach((issue) => {
           zodErrors.push(issue.path.join('.'))
-          if (this.appService.isDev()) {
-            console.log(
-              `# ZODERROR Propriedade inválida: ${issue.path.join('.')}`,
-              `VALUE: ${value}`,
-              `EXPECTED: ${(issue as { expected: string }).expected}`,
-              `RECEIVED: ${(issue as { received: string }).received}`
-            )
-          }
+          console.log(
+            `# ZODERROR Propriedade inválida: ${issue.path.join('.')}`,
+            `VALUE: ${value}`,
+            `EXPECTED: ${(issue as { expected: string }).expected}`,
+            `RECEIVED: ${(issue as { received: string }).received}`
+          )
         })
 
         throw new BadRequestException(
